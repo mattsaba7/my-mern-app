b9517bf60bed740d02d8d832820be764a8088eac
mattsaba7 <mattsabamusic@gmail.com>
2026-02-26 20:43:20 -0600

    feat: set up initial routing with HomePage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,17 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-   'mern stack'
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
