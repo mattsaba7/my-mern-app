b2b5088abc4e0167566af37a6a6a67e919adc43e
mattsaba7 <mattsabamusic@gmail.com>
2026-03-02 10:24:56 -0600

    Add public product pages and 404 route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,17 @@
+// External libraries
 import { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// Components/pages
 import HomePage from './pages/HomePage';
+import CartPage from './pages/CartPage';
+import LoginPage from './pages/LoginPage';
+import ProductDetailsPage from './pages/ProductDetailsPage';
+import ProductListPage from './pages/ProductListPage';
+import RegisterPage from './pages/RegisterPage';
+import NotFoundPage from './pages/NotFoundPage';
+
+// Styles
 
 function App() {
   const [count, setCount] = useState(0);
@@ -9,6 +20,13 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/product-list" element={<ProductListPage />} />
+        <Route path="/product-details" element={<ProductDetailsPage />} />
+        <Route path="/product-details/:id" element={<ProductDetailsPage />} />
+        <Route path="/cart" element={<CartPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
   );
