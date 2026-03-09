12f56464d10aeca15e38c47ed6ae16abd136cdfd
mattsaba7 <mattsabamusic@gmail.com>
2026-03-09 16:48:24 -0500

    feat: implement responsive navbar with search, routing, and user menu

--- client/src/components/HeaderComponent.jsx
+++ client/src/components/HeaderComponent.jsx
@@ -1,31 +1,99 @@
-import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap';
+import {
+  Navbar,
+  Nav,
+  Container,
+  NavDropdown,
+  Badge,
+  Form,
+  DropdownButton,
+  Dropdown,
+  Button,
+  InputGroup,
+} from 'react-bootstrap';
+
+import { LinkContainer } from 'react-router-bootstrap';
+import { Link } from 'react-router-dom';
 
 const HeaderComponent = () => {
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
       <Container>
-        <Navbar.Brand href="/">React-Bootstrap</Navbar.Brand>
-
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-
+        <LinkContainer to="/">
+          <Navbar.Brand href="/">Best Online Shop</Navbar.Brand>
+        </LinkContainer>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />{' '}
+        {/* what is this doing? What does it correspond to in my nav bar? */}
         <Navbar.Collapse id="basic-navbar-nav">
+          {' '}
+          {/* What is the Navbar.Collapse doing? */}
+          {/* Left Side Navigation */}
           <Nav className="me-auto">
-            <Nav.Link href="#">Link</Nav.Link>
-            <Nav.Link href="#">Link</Nav.Link>
-
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item>Action</NavDropdown.Item>
-              <NavDropdown.Item>Another action</NavDropdown.Item>
-              <NavDropdown.Item>Something else here</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item>Separated link</NavDropdown.Item>
-            </NavDropdown>
+            {' '}
+            {/* What is the Nav and InputGroup doing? */}
+            <InputGroup>
+              <DropdownButton id="dropdown-basic-button" title="All">
+                <Dropdown.Item>Electronics</Dropdown.Item>
+                <Dropdown.Item>Cars</Dropdown.Item>
+                <Dropdown.Item>Books </Dropdown.Item>
+              </DropdownButton>
+              <Form.Control type="text" placeholder="Normal text" />{' '}
+              {/* Is this basically a text field with placeholder text "Normal text"? */}
+              <Button variant="warning">
+                {' '}
+                {/* Is this basically a submit button for what you type into the text field? Why does it have a variant of warning? What is a variant? Also, is the variant an 
+              attribute or prop? */}
+                <i className="bi bi-search text-dark"></i>
+              </Button>
+            </InputGroup>
           </Nav>
-
           <Nav>
-            <Nav.Link href="#">Right Link</Nav.Link>
-            <Nav.Link href="#">Right Link</Nav.Link>
+            <LinkContainer to="/admin/orders">
+              <Nav.Link>
+                Admin
+                <span className="position-absolute top-1 start-10 translate-middle p-2 bg-danger border border-light rounded-circle"></span>
+              </Nav.Link>
+            </LinkContainer>
+
+            <NavDropdown title="John Doe" id="basic-nav-dropdown">
+              <NavDropdown.Item
+                as={Link}
+                eventKey="/user/my-orders"
+                to="/user/my-orders"
+              >
+                {' '}
+                {/* What does the eventKey do? */}
+                My Orders
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item as={Link} eventKey="/user" to="/user">
+                {' '}
+                {/* What does the eventKey do? */}
+                My profile
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item>
+                {' '}
+                {/* What does the eventKey do? */}
+                Logout
+              </NavDropdown.Item>
+            </NavDropdown>
+            <LinkContainer to="/login">
+              <Nav.Link>Login</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/register">
+              <Nav.Link>Register</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/cart">
+              <Nav.Link>
+                <Badge pill bg="danger">
+                  2
+                </Badge>
+                <i className="bi bi-cart-dash"></i>
+                <span className="ms-1">Cart</span>
+              </Nav.Link>
+            </LinkContainer>
           </Nav>
+          {/* Right Side Navigation */}
         </Navbar.Collapse>
       </Container>
     </Navbar>
