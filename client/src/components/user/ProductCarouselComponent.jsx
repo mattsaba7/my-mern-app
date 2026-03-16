8032a0d5529ae267a321867961de97c68d08c536
mattsaba7 <mattsabamusic@gmail.com>
2026-03-16 10:59:31 -0500

    feat: add homepage carousel and dynamic category cards

--- client/src/components/user/ProductCarouselComponent.jsx
+++ client/src/components/user/ProductCarouselComponent.jsx
@@ -1,43 +1,52 @@
 import Carousel from 'react-bootstrap/Carousel';
+import { LinkContainer } from 'react-router-bootstrap';
 
 function ProductCarouselComponent() {
   return (
     <Carousel>
       <Carousel.Item>
         <img
+          crossOrigin="anonymous"
           className="d-block w-100"
+          style={{ height: '300px', objectFit: 'cover' }}
           src="/images/carousel/carousel-1.png"
           alt="first slide"
         />
 
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
+        <LinkContainer to="/product-details">
+          <Carousel.Caption>
+            <h3>Bestseller in Laptops Category</h3>
+            <p>Dell Inspiron 15 3000 Laptop, 15.6 inch HD</p>
+          </Carousel.Caption>
+        </LinkContainer>
       </Carousel.Item>
       <Carousel.Item>
         <img
           className="d-block w-100"
+          style={{ height: '300px', objectFit: 'cover' }}
           src="/images/carousel/carousel-2.png"
           alt="second slide"
         />
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
+        <LinkContainer to="/product-details">
+          <Carousel.Caption>
+            <h3>Bestseller in Laptops Category</h3>
+            <p>Dell Inspiron 15 3000 Laptop, 15.6 inch HD</p>
+          </Carousel.Caption>
+        </LinkContainer>
       </Carousel.Item>
       <Carousel.Item>
         <img
           className="d-block w-100"
+          style={{ height: '300px', objectFit: 'cover' }}
           src="/images/carousel/carousel-3.png"
           alt="third slide"
         />
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
+        <LinkContainer to="/product-details">
+          <Carousel.Caption>
+            <h3>Bestseller in Cameras Category</h3>
+            <p>Check out our cameras.</p>
+          </Carousel.Caption>
+        </LinkContainer>
       </Carousel.Item>
     </Carousel>
   );
