8ee41f0e247899f6ca9aaf0f44fabe3325d9ef7d
mattsaba7 <mattsabamusic@gmail.com>
2026-03-20 12:18:02 -0500

    feat(filters): add rating filter component with selectable star options

--- client/src/components/ProductForListComponent.jsx
+++ client/src/components/ProductForListComponent.jsx
@@ -1,8 +1,9 @@
-import { Card, Button } from "react-bootstrap";
+import { Card, Button } from 'react-bootstrap';
+import { RatingView } from 'react-simple-star-rating';
 
 const ProductForListComponent = () => {
   return (
-    <Card style={{ width: "18rem" }}>
+    <Card style={{ width: '18rem' }}>
       <Card.Img variant="top" src="/images/tablets-category.png" />
       <Card.Body>
         <Card.Title>Card Title</Card.Title>
