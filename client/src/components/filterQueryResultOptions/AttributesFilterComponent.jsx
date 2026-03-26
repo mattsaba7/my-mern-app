5bb721f69b1e3c97252642f9b68f784125decf13
mattsaba7 <mattsabamusic@gmail.com>
2026-03-26 14:32:17 -0500

    refactor: improve filter options with dynamic rendering and styling

--- client/src/components/filterQueryResultOptions/AttributesFilterComponent.jsx
+++ client/src/components/filterQueryResultOptions/AttributesFilterComponent.jsx
@@ -1,16 +1,30 @@
-import { Form } from "react-bootstrap";
+import { Form } from 'react-bootstrap';
 
 const AttributesFilterComponent = () => {
+  const attributes = [
+    { name: 'Color', options: ['red', 'blue', 'green'] },
+    { name: 'RAM', options: ['1 TB', '2 TB'] },
+  ];
+
   return (
     <>
-      <Form.Label>Color</Form.Label>
-      <Form.Check 
-        type="checkbox"
-        id="default-checkbox"
-        label="green"
-      />
+      {attributes.map((item, index) => (
+        <div key={index}>
+          <Form.Label>{item.name}</Form.Label>
+
+          {item.options.map((option, optionIndex) => (
+            <Form.Check
+              key={optionIndex}
+              type="checkbox"
+              id={`${item.name}-${optionIndex}`}
+              label={option}
+            />
+          ))}
+        </div>
+      ))}
     </>
   );
 };
 
 export default AttributesFilterComponent;
+
